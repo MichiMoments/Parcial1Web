8252ce074e1617fe1a73e79f3a8e85b8e040df72
David Perez <d.perezc23@uniandes.edu.co>
2024-09-26 10:00:38 -0500

    LoginHecho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,75 @@
+import './Home.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+function Home() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    
+    const handleChange = (e) => {
+        setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+        });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!formData.email.includes('@')) {
+            alert('Email invalido, forma incorrecta');
+            return;
+        }
+        else if (!formData.password > 8) {
+            alert('Contrasenia invalida, menor a 8 caracteres');
+            return;
+        }
+        else {
+            <Navigate to="/about" replace={true} />
+            console.log("fuck")
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-box">
+            <h2>Log in</h2>
+
+            <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                type="text"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form>
+            
+            </div>
+        </div>
+    );
+}
+
+
+
+
+export default Home;
